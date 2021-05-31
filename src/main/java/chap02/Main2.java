49555fe9b55c2279088f141ad6a4c04fa496bfdc
ghysnzsy <30285849+ghysnzsy@users.noreply.github.com>
2021-05-31 21:44:23 +0900

    1차 정리

--- src/main/java/chap02/Main2.java
+++ src/main/java/chap02/Main2.java
@@ -5,7 +5,18 @@ public class Main2 {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppContext.class);
         Greeter g = ctx.getBean("greeter", Greeter.class);
         String msg = g.greet("스프링");
-        System.out.println("스프링");
+        System.out.println(msg);
+
+        Greeter g1 = ctx.getBean("greeter", Greeter.class);
+        Greeter g2 = ctx.getBean("greeter", Greeter.class);
+        System.out.println("(g1 == g2) = "+(g1 == g2));
+
+        Greeter g3 = ctx.getBean("greeter1", Greeter.class);
+        System.out.println("(g2 == g3) = "+(g2 == g3));
+        String msg2 = g3.greet("스프링");
+        System.out.println(msg2);
+
+
         ctx.close();
     }
 }
