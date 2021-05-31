764dbf6193efada653c34955d069287559a24add
ghysnzsy <30285849+ghysnzsy@users.noreply.github.com>
2021-05-31 21:34:21 +0900

    커밋테스트!

--- src/main/java/chap02/AppContext.java
+++ src/main/java/chap02/AppContext.java
@@ -1,2 +1,13 @@
-package chap02;public class AppContext {
+package chap02;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppContext {
+    @Bean
+    public Greeter greeter(){
+        Greeter g = new Greeter();
+        g.setFormat("%s, 안녕하세요!");
+        return g;
+    }
 }
--- src/main/java/chap02/Main2.java
+++ src/main/java/chap02/Main2.java
@@ -1,2 +1,11 @@
-package chap02;public class Main2 {
+package chap02;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+public class Main2 {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppContext.class);
+        Greeter g = ctx.getBean("greeter", Greeter.class);
+        String msg = g.greet("스프링");
+        System.out.println("스프링");
+        ctx.close();
+    }
 }
